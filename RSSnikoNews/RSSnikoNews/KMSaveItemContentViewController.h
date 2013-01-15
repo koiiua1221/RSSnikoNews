6dc303ff4b6a97286b16b416d5c70571585b3a75
koujimiura <koiiua1221@gmail.com>
2013-01-15 19:11:53 +0900

    webview分割し記事とtwitter画面を分けた（masterからのデグレード引き継いだままのコード）

--- RSSnikoNews/RSSnikoNews/KMSaveItemContentViewController.h
+++ RSSnikoNews/RSSnikoNews/KMSaveItemContentViewController.h
@@ -11,11 +11,20 @@
 
 @interface KMSaveItemContentViewController : UIViewController<UIWebViewDelegate>
 {
-    KMSaveItem*    _item;
+    KMSaveItem* _item;
     UIWebView* _webView;
-    id  __unsafe_unretained _delegate;
+    UIWebView* _webTwitterView;
+    NSMutableData* _downloadedData;
+    NSURLConnection* _connection;
+    int _networkState;
+    NSArray *_writings;
+    NSArray *_tweets;
+    NSArray *_imgs;
+    id __unsafe_unretained _delegate;
 }
+@property (nonatomic, readonly) int networkState;
 @property (nonatomic, retain) KMSaveItem* item;
 @property (unsafe_unretained) id delegate;
+@property (nonatomic, readonly) NSData* downloadedData;
 
 @end
